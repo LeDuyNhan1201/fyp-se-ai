d4abe9ca8babad318d36c306e0e45ca1d8a96eea
LeDuyNhan1201 <benlun1201@gmail.com>
2025-02-26 22:52:16 +0000

    chore: hot fix

--- smart-cv/services/notification/src/main/java/com/ben/smartcv/notification/NotificationApplication.java
+++ smart-cv/services/notification/src/main/java/com/ben/smartcv/notification/NotificationApplication.java
@@ -1,11 +1,17 @@
 package com.ben.smartcv.notification;
 
+import com.ben.smartcv.common.config.OpenApiConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.ben.smartcv.common", "com.ben.smartcv.notification" })
+@ComponentScan(
+		basePackages = { "com.ben.smartcv.common", "com.ben.smartcv.notification" },
+		excludeFilters = {
+				@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = OpenApiConfig.class)
+})
 public class NotificationApplication {
 
 	public static void main(String[] args) {
