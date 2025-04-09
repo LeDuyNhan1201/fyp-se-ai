6f9759d851095f5cfdf0300572f06084105dd49f
LeDuyNhan1201 <benlun99999@gmail.com>
2025-04-09 12:58:27 +0700

    refactor: remove unused code of data-processor module

--- smart-cv/data-processor/job_processor.py
+++ smart-cv/data-processor/job_processor.py
@@ -1,8 +1,6 @@
 from application.job_service_grpc import job_processor_serve
-# from infrastructure.elasticsearch_client import ElasticsearchClient
 
 def main():
-    # elasticsearch_client = ElasticsearchClient()
     job_processor_serve()
 
 if __name__ == '__main__':
